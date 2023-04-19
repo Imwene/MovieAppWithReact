ed3a9d9143ab417d0ea2a6c7189eefc4df27d19b
Dev01 <brimwene@gmail.com>
2023-04-19 16:40:13 +0300

    Render all movies parsed in search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import {useEffect} from 'react';
+//import useState and useEffect
+import {useState, useEffect} from 'react';
 import MovieCard from './MovieCard';
 
 import './App.css';
 import searchIcon from './search.svg';
 
+//omdb API URL
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=643a6484';
 
-const movie1 = {
+const movie = {
   "Title": "The Amazing Spiderman 2 Webb Cut",
   "Year": "2021",
   "imdbID": "tt18351128",
@@ -15,18 +17,21 @@ const movie1 = {
 }
 
 const App = () => {
-
+  //useEffect
+  const [movies, setMovies] = useState([]);
+  //search using async await
   const searchMovies = async (title) => {
       const response = await fetch(`${API_URL}&s=${title}`);
       const data = await response.json();
 
-      console.log(data);
+      setMovies(data.Search);
   }
-
+  //useEffect with dependancy array
   useEffect(() => {
-      searchMovies('Spiderman');
+      searchMovies('Bourne');
   }, [])
 
+  //jsx wrapped in a single div element
   return (
     <div className="app">
       <h1>Imwene Movie Land</h1>
@@ -36,10 +41,23 @@ const App = () => {
         </input>
         <img src={searchIcon} alt='search' onClick={()=>{}} />
       </div>
+    {/*Map through the movie and render movie card for each iteration of movie*/}
+      {
+        movies.length > 0 ? (
+          <div  className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+                  
+          </div>
+        ) : (
+          <div>
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
 
-      <div  className="container">
-        <MovieCard movie1={movie1} />        
-      </div>
+      
 
     </div>
   );
